55aeef04be2a5e9a5d39e15a00bbfc172aaa29ca
raijin <raijin@kali-razer>
2025-12-05 15:58:31 +0700

    fix port 5000 flask error4

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
   server: {
-    port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
@@ -20,7 +13,7 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
-    emptyOutDir: true,
+    sourcemap: false,
   },
 })
 
